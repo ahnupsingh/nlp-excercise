3fff960153ef72894e86d0cc1bddfec6ab90d1dc
Anup Singh <anup.singh2071@gmail.com>
2024-07-02 21:29:03 +0545

    chore: add new requirements

--- aquarius/models.py
+++ aquarius/models.py
@@ -12,3 +12,17 @@ class City(models.Model):
     class Meta:
         verbose_name = 'City'
         verbose_name_plural = 'Cities'
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def verbose_name(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'City'
+        verbose_name_plural = 'Cities'
